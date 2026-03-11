64dbfdda0ea68b58ddb79684dce88cbaa1323bad
Nagham Sabbour <nagham.sabbour@gmail.com>
2026-03-11 12:14:40 -0400

    Preprocessing the ShapeNet Part Dataset to be used for training. Done in the ipynb and then extracted into the python files.

---
+++ src/config.py
@@ -0,0 +1,10 @@
+import os
+
+DATA_ROOT = os.path.expanduser(
+    "./data/shapenetcore_partanno_segmentation_benchmark_v0_normal"
+)
+
+NUM_POINTS = 1024
+BATCH_SIZE = 20
+USE_NORMALS = False
+RANDOM_SEED = 42
